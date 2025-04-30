1db930cd0fe0d9010eaa838432538ed0d1bfe718
Manojchary <108991891+Manojchary@users.noreply.github.com>
2025-04-30 18:58:42 +0530

    Update AS_T_2.sql

--- AS_T_2.sql
+++ AS_T_2.sql
@@ -1,14 +1,15 @@
--- DEPARTMENTS: Stores department-level data for grouping employees 
-CREATE DATABASE EMPLOYEE_DB;
+-- CREATING DATABASE FOR EMPLOYEES
+CREATE DATABASE EMPLOYEES_DB;
 USE EMPLOYEE_DB;
 
+-- DEPARTMENTS: Stores department-level data for grouping employees 
 CREATE TABLE departments_table(
     dept_id INT AUTO_INCREMENT PRIMARY KEY,
     dept_name VARCHAR(100) NOT NULL UNIQUE
 );
 
 -- EMPLOYEES: Stores employee personal and job-related INFO
-CREATE TABLE employees_table (
+CREATE TABLE employees_table(
     emp_id INT AUTO_INCREMENT PRIMARY KEY,
     full_name VARCHAR(100) NOT NULL,
     email_id VARCHAR(100) UNIQUE NOT NULL,
@@ -38,26 +39,26 @@ CREATE TABLE salary_records_table (
 );
 
 -- INDEXES for fast queries on common filters
-CREATE INDEX idx_emp_email ON employees(email);
-CREATE INDEX idx_salary_month ON salary_records(salary_month);
-CREATE INDEX idx_emp_month ON salary_records(emp_id, salary_month);
+CREATE INDEX idx_emp_email_id ON employees_table(email);
+CREATE INDEX idx_salary_month ON salary_records_table(salary_month);
+CREATE INDEX idx_emp_month ON salary_records_table(emp_id, salary_month);
 
 
 -- Insert departments values 
-INSERT INTO departments_table (dept_name)
+INSERT INTO departments_table(dept_name)
 VALUES 
 ('Human Resources'),
 ('Finance'),
 ('Engineering');
 
 -- Insert employees values
-INSERT INTO employees_table (full_name, email, phone_number, hire_date, job_title, dept_id)
+INSERT INTO employees_table(full_name, email_id, phone_no, hire_date, job_title, dept_id)
 VALUES
 ('Rahul Mehra', 'rahul.mehra@example.com', '9998812345', '2023-06-01', 'Software Engineer', 3),
 ('Sneha Iyer', 'sneha.iyer@example.com', '9998876543', '2022-11-15', 'HR Manager', 1);
 
 -- Insert salary records values
-INSERT INTO salary_records_table (emp_id, salary_month, basic_salary, bonus, deductions)
+INSERT INTO salary_records_table(emp_id, salary_month, basic_salary, bonus, deductions)
 VALUES
 (1, '2025-04-01', 60000, 5000, 2000),
 (2, '2025-04-01', 50000, 3000, 1000);
